84e802045ff17bc547a2435ef19f184aa0583c43
unknown <longcanrong@gmail.com>
2024-03-20 15:08:10 +0800

    update

--- Practical6/Average day.py
+++ Practical6/Average day.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-time = {'sleeping' : 8 , 'classes' : 6 , 'studying' : 3.5 , 'TV' : 2 , 'music' : 1 } 
-time['other']= 24 - sum(time.values())
-print(time)
+time = {'sleeping' : 8 , 'classes' : 6 , 'studying' : 3.5 , 'TV' : 2 , 'music' : 1 } #input the data
+time['other']= 24 - sum(time.values()) #get the time of 'others'
+print(time) #output
 print(time['sleeping'])
 
 plt.figure()
-plt.pie(time.values(), labels= time.keys())
+plt.pie(time.values(), labels= time.keys()) #make a pie chart
 plt.show()
 plt.clf()
