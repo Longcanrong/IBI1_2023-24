0803f5cce3f2e8f78252a92bd8a6d2034746fd9a
Canrong Long <canrong.23@intl.zju.edu.cn>
2024-05-21 11:47:24 +0800

    update the practical13 to final version

--- Practical13/alignment.py
+++ Practical13/alignment.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def read_sequence(file_path):
     seq=''
@@ -7,15 +8,28 @@ def read_sequence(file_path):
         if line.startswith('>'):
             continue
         else:
-            seq+=line
+            seq+=line.strip()
     return seq
 
-def calculate_score(seq1, seq2):
-    hamming_distance=0			
-    for	i in range(len(seq1)):
-        if	seq1[i]!=seq2[i]:				
-            hamming_distance+=1
-    return hamming_distance
+df = pd.read_csv('BLOSUM62.csv', index_col=0, header=0)
+
+blosum62_matrix = df.to_dict('index')
+
+# def calculate_score(seq1, seq2, blosum62_matrix):
+#     score = 0
+#     min_len = min(len(seq1), len(seq2))
+#     # Ensure the two sequences are in the same length
+#     for i in range(min_len):
+#         aa1, aa2 = seq1[i], seq2[i]
+#         score += blosum62_matrix.get(aa1, {}).get(aa2, 0)
+#     # Define the calculate measures
+#     return score
+
+def calculate_score(seq1, seq2, blosum62_matrix):
+    score = 0
+    for aa1, aa2 in zip(seq1, seq2):
+        score += blosum62_matrix[aa1][aa2]
+    return score
 
 def calculate_identity(seq1, seq2):
     identical = sum(1 for a, b in zip(seq1, seq2) if a == b)
@@ -24,8 +38,8 @@ def calculate_identity(seq1, seq2):
 seq1 = read_sequence('SLC6A4_HUMAN.fa')
 seq2 = read_sequence('SLC6A4_MOUSE.fa')
 
-score = calculate_score(seq1, seq2)
+score = calculate_score(seq1, seq2, blosum62_matrix)
 identity = calculate_identity(seq1, seq2)
 
-print(f"Alignment Score (hamming distance): {score}")
+print(f"Alignment Score: {score}")
 print(f"Percentage of Identity: {identity}%")
